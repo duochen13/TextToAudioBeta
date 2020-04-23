025f21da9ef6951b1892044a9791365dad84215d
duochen13 <duochen@umich.edu>
2020-04-23 12:22:14 -0400

    add camera access, image picker

--- ListenText/ViewController.swift
+++ ListenText/ViewController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var imagePicker: UIImagePickerController!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .camera
+        //imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated:  true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageView.image = info[.originalImage] as? UIImage
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
