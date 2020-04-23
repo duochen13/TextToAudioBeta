c973597e7fce769a4a3065b727e04baad864e192
duochen13 <duochen@umich.edu>
2020-04-23 13:48:35 -0400

    add gcp vision api

--- ListenText/ViewController.swift
+++ ListenText/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import AVFoundation
+import SwiftyJSON
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     var imagePicker: UIImagePickerController!
+    var googleAPIKey = ""
+    var googleURL: URL {
+        return URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(googleAPIKey)")!
+    }
     
     @IBOutlet weak var imageView: UIImageView!
     
@@ -19,15 +24,66 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         
-        imagePicker.sourceType = .camera
-        //imagePicker.sourceType = .photoLibrary
+        //imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         
         present(imagePicker, animated:  true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = info[.originalImage] as? UIImage
+            
+            // Encode the image (result is String)
+            let binaryImageData = base64EncodeImage(pickedImage)
+            createRequest(with: binaryImageData)
+        }
+        
         imagePicker.dismiss(animated: true, completion: nil)
-        imageView.image = info[.originalImage] as? UIImage
+    
+        // send image to server
+        
+    }
+    
+    func base64EncodeImage(_ image: UIImage) -> String {
+        let imagedata = image.pngData()
+        // Resize the image if it exceeds the 2MB API limit, to be added
+        return imagedata!.base64EncodedString(options: .endLineWithCarriageReturn)
+    }
+    
+    func createRequest(with imageBase64: String) {
+        var request = URLRequest(url: googleURL)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(Bundle.main.bundleIdentifier ?? "", forHTTPHeaderField: "X-Ios-Bundle-Identifier")
+        // Build our API request
+        let jsonRequest = [
+            "requests": [
+                "image": [
+                    "content": imageBase64
+                ],
+                "features": [
+                    [
+                        "type": "LABEL_DETECTION",
+                        "maxResults": 10
+                    ],
+                    [
+                        "type": "FACE_DETECTION",
+                        "maxResults": 10
+                    ]
+                ]
+            ]
+        ]
+        let jsonObject = JSON(jsonDictionary: jsonRequest)
+        
+        // Serialize the JSON
+        guard let data = try? jsonObject.rawData() else {
+            return
+        }
+        request.httpBody = data
+        // Run the request on a background thread
+        DispatchQueue.global().async { self.runRequestOnBackgroundThread(request) }
     }
     
     override func viewDidLoad() {
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'ListenText' do
+    pod 'SwiftyJSON', '~> 3.1'
+end
+
