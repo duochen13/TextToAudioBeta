6beb3968fcd5f4216c2b888b8c7b7e8fd173e99d
duochen13 <duochen@umich.edu>
2020-05-04 23:31:21 -0400

    add nltk

--- nlp/nn.py
+++ nlp/nn.py
@@ -1,61 +1,13 @@
-import numpy as np
-import math
-
-import numpy as np
-
-def sigmoid(x):
-    return 1.0/(1+ np.exp(-x))
-
-def sigmoid_derivative(x):
-    return x * (1.0 - x)
-
-class NeuralNetwork:
-    def __init__(self, x, y):
-        self.input      = x
-        self.weights1   = np.random.rand(self.input.shape[1],4) 
-        self.weights2   = np.random.rand(4,1)                 
-        self.y          = y
-        self.output     = np.zeros(self.y.shape)
-
-    def feedforward(self):
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = sigmoid(np.dot(self.layer1, self.weights2))
-
-    def backprop(self):
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
-
-        # update the weights with the derivative (slope) of the loss function
-        self.weights1 += d_weights1
-        self.weights2 += d_weights2
+import pandas as pd
+import tensorflow as tf
+import nltk
+from matplotlib import pyplot as plt
+from nltk import sent_tokenize
 
 
-if __name__ == "__main__":
-    X = np.array([[0,0,1],
-                  [0,1,1],
-                  [1,0,1],
-                  [1,1,1]])
-    y = np.array([[0],[1],[1],[0]])
-    nn = NeuralNetwork(X,y)
+content = '''Introduction to Mina Mina Myoui (2# Myõi Mina), known mostly as Mina (Hangul: 014), was born on March 24, 1997 in San Antonio, Texas, and moved to Kobe, Japan, when she was a toddler. She is a vocalist and lead dancer of the group. In 2013, she was scouted by a JYP Entertainment staff member while shopping with her mother and was invited to join JYP Entertainment Global. She auditioned in a JYP audition in Japan and joined the trainee program in South Korea on January 2, 2014. Age 23 Debut Oct 20, 2015 '''
 
-    for i in range(1000):
-        nn.feedforward()
-        nn.backprop()
+# nltk.download('punkt')
+sentences = sent_tokenize(content, language='en')
 
-    print(nn.output)
-
-#  https://towardsdatascience.com/understand-text-summarization-and-create-your-own-summarizer-in-python-b26a9f09fc70
-# abstract (attention layer)
-# https://www.analyticsvidhya.com/blog/2019/06/comprehensive-guide-text-summarization-using-deep-learning-python/
-# https://github.com/aravindpai/How-to-build-own-text-summarizer-using-deep-learning/blob/master/How_to_build_own_text_summarizer_using_deep_learning.ipynb
-
-
-# GREAT article：
-# https://hackernoon.com/text-summarizer-using-deep-learning-made-easy-490880df6cd?source=post_stats_page---------------------------
-
-
-# other feature
-# analysis avg sentence length
-
-# generate a graph shows the performance of article
+print(sentences)
